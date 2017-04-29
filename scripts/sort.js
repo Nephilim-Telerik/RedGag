cec7eab4918796048fa1a1e8d3b9d7ea04a7689a
boda66 <boda66@abv.bg>
2017-04-29 16:49:00 +0300

    added event listeners for the buttons: Hot, Recent, Trending.

---
+++ scripts/sort.js
@@ -0,0 +1,94 @@
+var i,j;
+var recent = document.getElementById("recent");
+var hot = document.getElementById("hot");
+var trending = document.getElementById("trending");
+
+recent.addEventListener("click", Recent);
+
+function Recent() {
+    var tempRecent;
+    for(i=0; i<array.arr.length; i=i+1)
+    {
+        for(j=0; j<array.arr.length; j=j+1)
+        {
+            if(array.arr[i].date > array.arr[j].date)
+            {
+                tempRecent = array.arr[i];
+                array.arr[i] = array.arr[j];
+                array.arr[j] = tempRecent;
+            }
+        }
+    }
+    func(array);
+    hot = document.getElementById("hot");
+    hot.addEventListener("click", Hot);
+    trending = document.getElementById("trending");
+    trending.addEventListener("click", Trending);
+}
+
+
+
+
+hot.addEventListener("click", Hot);
+
+function Hot() {
+    var tempHot;
+    for(i=0; i<array.arr.length; i=i+1)
+    {
+        for(j=0; j<array.arr.length; j=j+1)
+        {
+            if(array.arr[i].rating > array.arr[j].rating)
+            {
+                tempHot = array.arr[i];
+                array.arr[i] = array.arr[j];
+                array.arr[j] = tempHot;
+            }
+        }
+    }
+    var arr = {arr: []};
+
+    for(i=0; i<3; i=i+1)
+    {
+        
+            arr.arr.push(array.arr[i]);
+        
+    }
+    func(arr);
+    recent = document.getElementById("recent");
+    recent.addEventListener("click", Recent);
+    trending = document.getElementById("trending");
+    trending.addEventListener("click", Trending);
+
+}
+
+trending.addEventListener("click", Trending);
+
+function Trending() {
+    var tempTrending;
+    for(i=0; i<array.arr.length; i=i+1)
+    {
+        for(j=0; j<array.arr.length; j=j+1)
+        {
+            if(array.arr[i].rating > array.arr[j].rating)
+            {
+                tempTrending = array.arr[i];
+                array.arr[i] = array.arr[j];
+                array.arr[j] = tempTrending;
+            }
+        }
+    }
+    var arr = {arr: []};
+
+    for(i=0; i<array.arr.length; i=i+1)
+    {
+        if(array.arr[i].rating >= 10)
+        {
+            arr.arr.push(array.arr[i]);
+        }
+    }
+    func(arr);
+    recent = document.getElementById("recent");
+    recent.addEventListener("click", Recent);
+    hot = document.getElementById("hot");
+    hot.addEventListener("click", Hot);
+}
