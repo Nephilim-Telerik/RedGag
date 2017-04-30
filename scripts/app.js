c50c40c851a84a97610195c46bf4e5bdff4ab59d
petkoche <petkoche90@gmail.com>
2017-04-30 16:17:33 +0300

    wrapping the db request in a promise
    providing db data with a promise

--- scripts/app.js
+++ scripts/app.js
@@ -1,22 +1,29 @@
- 
-  (function() {
-  // Initialize Firebase
-  var config = {
-    apiKey: "AIzaSyAWgpPRCP49mlZbgFpEbH1AVf_otxQJlMc",
-    authDomain: "redgag-e9119.firebaseapp.com",
-    databaseURL: "https://redgag-e9119.firebaseio.com",
-    projectId: "redgag-e9119",
-    storageBucket: "redgag-e9119.appspot.com",
-    messagingSenderId: "243757547322"
-  };
-  firebase.initializeApp(config);
+var posts;
+var promiseDataBasePosts = new Promise(function (resolve, reject) {
+  (function () {
+    // Initialize Firebase
+    var config = {
+      apiKey: "AIzaSyAWgpPRCP49mlZbgFpEbH1AVf_otxQJlMc",
+      authDomain: "redgag-e9119.firebaseapp.com",
+      databaseURL: "https://redgag-e9119.firebaseio.com",
+      projectId: "redgag-e9119",
+      storageBucket: "redgag-e9119.appspot.com",
+      messagingSenderId: "243757547322"
+    };
+    firebase.initializeApp(config);
 
-  const dbRefObject = firebase.database().ref().child('object');
+    const dbRefObject = firebase.database().ref().child('object');
 
-  dbRefObject.on('value', snap => {
-   // preObject.innerText = JSON.parse(JSON.stringify(snap.val(), null, 3));
-   var posts = JSON.parse(JSON.stringify(snap.val()));
-   console.log(posts);
-  });
-
-  }());
+    dbRefObject.on('value', snap => {
+      posts = JSON.parse(JSON.stringify(snap.val()));
+      resolve(posts);
+    });
+  })();
+});
+promiseDataBasePosts
+        .then(function(posts) {
+            console.log(posts);
+        })
+        .catch(function(error) {
+            console.log(error);
+    });
